468a242cee4e1c8de7b38c32fc217754fece6984
Christian Diaz <rivalch2015@gmail.com>
2025-02-11 21:53:48 -0400

    Creamos props para que hacer dinamicos los componentes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
-import logo from './platzi.webp';
 import { TodoCounter } from './TodoCounter';
 import { TodoSearch } from './TodoSearch';
 import { TodoList } from './TodoList';
 import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
 import './App.css';
+import React from 'react';
+
+const defaultTodos = [
+  {text: 'Cortar cebolla', completed: true},
+  {text: 'Tomar el curso de Intro a React.s', completed: false},
+  {text: 'Otra cosa', completed: false},
+  {text: 'Otra cosa 2', completed: false},
+]
 
 function App() {
   return (
-    <div className="App">
+    <React.Fragment>
 
-      <TodoCounter />
+      <TodoCounter completed={16} total={25} />
       <TodoSearch />
       
       <TodoList>
 
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {defaultTodos.map(todo=> (
+          <TodoItem 
+            key={todo.text} 
+            text={todo.text}
+            completed = {todo.completed}
+          />
+        ))}
+
       </TodoList>      
 
       <CreateTodoButton/> 
-    </div>
+    </React.Fragment>
   );
 }
 
