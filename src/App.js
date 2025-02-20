e5f0053949c29ab1f32018f640d383033e56789c
Christian Diaz <rivalch2015@gmail.com>
2025-02-19 22:04:20 -0400

    Agregamos los estilos css en todos los componentes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import { TodoSearch } from './TodoSearch';
 import { TodoList } from './TodoList';
 import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
-import './App.css';
 import React from 'react';
 
 const defaultTodos = [
   {text: 'Cortar cebolla', completed: true},
   {text: 'Tomar el curso de Intro a React.s', completed: false},
-  {text: 'Otra cosa', completed: false},
+  {text: 'Otra cosa', completed: true},
   {text: 'Otra cosa 2', completed: false},
 ]
 
