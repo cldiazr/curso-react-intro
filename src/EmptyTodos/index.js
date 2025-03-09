51200636164c2d01562ba5e20292e3df9e0fe44e
Christian Diaz <rivalch2015@gmail.com>
2025-03-08 22:28:04 -0400

    Creamos componentes de carga utilizando loadng skeletons para mejorar la experecia de usuario

---
+++ src/EmptyTodos/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function EmptyTodos () {
+
+    return(
+        <p>No tienes TODOS creados</p>
+    )
+  }
+
+export { EmptyTodos }
---
+++ src/TodosError/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function TodosError () {
+
+    return(
+        <p>Tenemos un Error</p>
+    )
+  }
+
+export { TodosError }
